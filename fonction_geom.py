9965408c602a2290947443b36d3b12e9e2b0de96
ei-se <yalakhdhar@gmail.com>
2023-02-03 12:29:32 +0100

    probably geom achieved

--- fonction_geom.py
+++ fonction_geom.py
@@ -1,2 +1,13 @@
 def isGeom(liste):
-    pass
+    if len(liste) < 2 or liste[0] == 0:
+        return False
+    else:
+        raison = liste[1]/liste[0] #Raison hypothetique de la suite
+        lng = len(liste) #longueur de la liste
+        for i in range(lng-1):
+            if liste[i] == 0: #On verifie qu'on a pas de divisions par 0
+                return False
+            else:
+                if liste[i+1]/liste[i] != raison: #On verifie que l'ecart est egale a l'hypothetique raison
+                    return False
+        return True #Si tous les ecarts sont verifies, on renvoie alors True
