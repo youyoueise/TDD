8a6c723c600e816f26cf766dd9943996ce3b0c7f
ei-se <yalakhdhar@gmail.com>
2023-02-03 12:36:42 +0100

    Wrong example

--- test_geom.py
+++ test_geom.py
@@ -6,7 +6,7 @@ class testArith(unittest.TestCase):
         self.assertEqual(isGeom([]),False)
         self.assertEqual(isGeom([3]),False)
         self.assertEqual(isGeom([2,4,8]),True)
-        self.assertEqual(isArith([4,8,0]),False)
+        self.assertEqual(isGeom([4,8,0]),False)
         self.assertEqual(isGeom([6,12,24,48]),True)
 
 
