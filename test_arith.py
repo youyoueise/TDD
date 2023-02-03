3a36b99dd4881885051e38e2fbeb81f71dbe23d1
ei-se <yalakhdhar@gmail.com>
2023-02-03 12:34:24 +0100

    Wrong example

--- test_arith.py
+++ test_arith.py
@@ -5,7 +5,7 @@ class testArith(unittest.TestCase):
     def test_arith(self):
         self.assertEqual(isArith([4,12,43]),False)
         self.assertEqual(isArith([1]),False)
-        self.assertEqual(isArith([2,4]),True)
+        self.assertEqual(isArith([2,4,6]),True)
         self.assertEqual(isArith([1,2,3]),True)
         self.assertEqual(isArith([]),False)
 
