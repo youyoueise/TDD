ec502a03e7b89edfa448a0ba099ef58af20984e5
ei-se <yalakhdhar@gmail.com>
2023-02-03 13:02:41 +0100

    gen_arith probably achieved

--- fonction_gen_arith.py
+++ fonction_gen_arith.py
@@ -1,2 +1,9 @@
+from fonction_arith import *
+
 def genArith(liste,n):
-    pass
+    if isArith(liste) == False:
+        return False
+    else:
+        raison = liste[1] - liste[0]
+        new_liste = [liste[-1] + raison*(i+1) for i in range(n)]
+        return [True,new_liste]
