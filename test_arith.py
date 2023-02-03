ecd7746581245a8c951f267181744edb44df40a5
ei-se <yalakhdhar@gmail.com>
2023-02-03 11:56:38 +0100

    test_arith achieved

--- test_arith.py
+++ test_arith.py
@@ -3,7 +3,10 @@ from fonction_arith import *
 
 class testArith(unittest.TestCase):
     def test_arith(self):
-        self.assertEqual(isArith([1,2]),True)
+        self.assertEqual(isArith([2,4]))
+        self.assertEqual(isArith([1,2,3]),True)
+        self.assertEqual(isArith([]),False)
+        self.assertEqual(isArith(["Miam","Manger"]),False)
 
 
 if __name__ == '__main__':
