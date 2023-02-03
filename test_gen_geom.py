7175d1a994ecf4f11ad4ab8026fef2df1c8b25e7
ei-se <yalakhdhar@gmail.com>
2023-02-03 13:27:55 +0100

    wrong example

--- test_gen_geom.py
+++ test_gen_geom.py
@@ -3,7 +3,7 @@ from fonction_gen_geom import *
 
 class testGenGeom(unittest.TestCase):
     def test_gen_geom(self):
-        self.assertEqual(genGeom([2,4,8],3),[True,[8,10,12]])
+        self.assertEqual(genGeom([2,4,8],3),[True,[16,32,64]])
         self.assertEqual(genGeom([1,2,5],4),False)
         self.assertEqual(genGeom([2],6),False)
 
